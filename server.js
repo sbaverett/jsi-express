7ab25768cae1499946e176f2386162ae8dbd919c
Sam Averett <averettsam@comcast.net>
2014-06-17 16:12:20 -0700

    added get but not finished

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ app.use(require('method-override')());
 app.use(express.static(__dirname + '/public'));
 
 
-var people = {};
+var people = {
+  1: { id: 1, name: 'Adam' },
+  2: { id: 2, name: 'Ariel' },
+  3: { id: 3, name: 'Sam' },
+  4: { id: 4, name: 'Grant' }
+};
+
+// app.delete("/api/people/:id", function(req, res) {
+// 	console.log(people[req.params.id];
+// 		res.json({ status: deleted ? 'deleted' ok}))
+// })
+
+app.get('/api/people/:id', function(req, res) {
+	var person = people[req.params.id];
+	console.log(person);
+	
+})
 
 app.post('/api/people', function(req, res) {
   var id = people.length + 1;
